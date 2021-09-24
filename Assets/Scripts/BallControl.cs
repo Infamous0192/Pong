2fc1817627c79a9a2dce0626bfd0df0ee18e605e
Dwa Meizadewa <infamous0192@gmail.com>
2021-09-24 14:55:50 +0800

    Membuat kecepatan bola konstan

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -8,14 +8,24 @@ public class BallControl : MonoBehaviour
     public float xInitialForce;
     public float yInitialForce;
 
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        trajectoryOrigin = transform.position;
+
+        // Mulai game
+        RestartGame();
+    }
+
     // Untuk mengakses informasi titik asal lintasan
     public Vector2 TrajectoryOrigin
     {
         get { return trajectoryOrigin; }
     }
-
-    // Rigidbody 2D bola
-    private Rigidbody2D rigidBody2D;
 
     // Titik asal lintasan bola saat ini
     private Vector2 trajectoryOrigin;
@@ -37,9 +47,6 @@ public class BallControl : MonoBehaviour
 
     void PushBall()
     {
-        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
-        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
-
         // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
         float randomDirection = Random.Range(0, 2);
 
@@ -48,11 +55,11 @@ public class BallControl : MonoBehaviour
         if (randomDirection < 1.0f)
         {
             // Gunakan gaya untuk menggerakkan bola ini.
-            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yInitialForce));
         }
         else
         {
-            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yInitialForce));
         }
     }
 
@@ -64,15 +71,4 @@ public class BallControl : MonoBehaviour
         // Setelah 2 detik, berikan gaya ke bola
         Invoke("PushBall", 2);
     }
-
-    void Start()
-    {
-        rigidBody2D = GetComponent<Rigidbody2D>();
-
-        trajectoryOrigin = transform.position;
-
-        // Mulai game
-        RestartGame();
-    }
-
 }
